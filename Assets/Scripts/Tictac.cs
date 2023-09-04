ec40ed1fb789b4321b3378d3c00d09d6b9b96124
Prarup Regmi <prarupregmi@gmail.com>
2023-09-04 09:35:17 +0545

    Add README.md.

---
+++ Assets/Scripts/Tictac.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tictac : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Creating a start position with smooth transition from 0 to position
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
